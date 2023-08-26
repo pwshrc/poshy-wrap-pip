6a44a5343c946d002150d77b23c0fbd9f1eb1992
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-25 21:55:40 -0400

    ✨ Updates Module Implementation

--- src/poshy-wrap-pip.psm1
+++ src/poshy-wrap-pip.psm1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
-if (-not (Test-Command pip)) {
+if (-not (Test-Command pip) -and (-not $Global:PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED)) {
     return
 }
 
